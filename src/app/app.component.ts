78c0fb62c9d06a535ab5482dba43e356e438d1b7
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-03 08:00:06 -0800

    Change the app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{name}} details!</h2>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = "Tour of Heros";
+  name = "Iron Man";
+}
