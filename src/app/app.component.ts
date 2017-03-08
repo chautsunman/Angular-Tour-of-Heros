4870c27dc713895be6031fe343d4abfe8f26f1a6
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-08 08:01:45 -0800

    List the heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 
+
 export class Hero {
   id: number;
   name: string;
 }
 
+
+const HEROES: Hero[] = [
+  {id: 1, name: "Iron Man"},
+  {id: 2, name: "Captain America"},
+  {id: 3, name: "Thor"},
+  {id: 4, name: "Hulk"},
+  {id: 5, name: "Black Widow"},
+  {id: 5, name: "Hawkeye"}
+];
+
+
 @Component({
   selector: 'my-app',
   template: `
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
@@ -18,6 +37,7 @@ export class Hero {
   `,
 })
 export class AppComponent {
+  heroes = HEROES
   title = "Tour of Heros";
   hero: Hero = {
     id: 1,
