1d52f9129b0f18ab0f8530c599dec9d249132a85
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-08 08:10:24 -0800

    Display the detail information of the selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,17 @@ const HEROES: Hero[] = [
   template: `
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
 
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
     <div>
       <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
     </div>
   `,
   styles: [`
@@ -86,10 +86,12 @@ const HEROES: Hero[] = [
 `]
 })
 export class AppComponent {
-  heroes = HEROES
+  heroes = HEROES;
+
   title = "Tour of Heros";
-  hero: Hero = {
-    id: 1,
-    name: "Iron Man"
-  };
+  selectedHero = Hero;
+
+  onSelect(hero : Hero) : void {
+    this.selectedHero = hero;
+  }
 }
