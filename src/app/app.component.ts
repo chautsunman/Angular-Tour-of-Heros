7dec36b53d0068428e6517fc6f58e781ca026bf7
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-16 16:25:32 -0700

    Refactor the master/detail view into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import {Hero} from './hero';
 
 
 const HEROES: Hero[] = [
@@ -27,15 +22,7 @@ const HEROES: Hero[] = [
       </li>
     </ul>
 
-    <div *ngIf="selectedHero">
-      <h1>{{title}}</h1>
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
   .selected {
