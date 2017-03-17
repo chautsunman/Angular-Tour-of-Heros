103218caeb8280aadb3fc0e90862e98d62de290f
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-16 17:43:33 -0700

    Get heroes for the dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,10 +1,20 @@
 import {Component} from '@angular/core';
+import {OnInit} from '@angular/core';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   moduleId: module.id,
   selector: 'dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
-  
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroService.getHeros().then(heroes => this.heroes = heroes.slice(0, 4));
+  }
 }
